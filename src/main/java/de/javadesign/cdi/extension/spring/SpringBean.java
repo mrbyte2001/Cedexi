8bc6654f1c13928dbef6907fbc991a064c18289d
Dirk Dreyer <promedi@t-online.de>
2011-01-08 14:16:39 +0100

    add first implementation

---
+++ src/main/java/de/javadesign/cdi/extension/spring/SpringBean.java
@@ -0,0 +1,150 @@
+/*
+ * Copyright (C) 2010 Dirk Dreyer.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance
+ * with the License. * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to
+ * in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.
+ * See the License for the specific language governing permissions and limitations under the License.
+ */
+package de.javadesign.cdi.extension.spring;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
+import java.text.MessageFormat;
+import java.util.Collections;
+import java.util.Set;
+
+import javax.enterprise.context.Dependent;
+import javax.enterprise.context.spi.CreationalContext;
+import javax.enterprise.inject.spi.Bean;
+import javax.enterprise.inject.spi.InjectionPoint;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+
+/**
+ * A SpringBean object which implement Bean<?> from injection spi.
+ * 
+ * @author Dirk Dreyer
+ */
+public class SpringBean implements Bean<Object> {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger("de.javadesign.cdi.extension");
+
+    private String beanName;
+
+    private Class<?> beanClass;
+
+    private Set<Type> beanTypes;
+
+    private Set<Annotation> qualifiers;
+
+    private Set<Class<? extends Annotation>> stereotypes;
+
+    private ConfigurableBeanFactory beanFactory;
+
+    /**
+     * Constructor.
+     * 
+     * @param beanName
+     *            the bean name
+     * @param beanClass
+     *            the bean class
+     * @param beanTypes
+     *            the bean types
+     * @param qualifiers
+     *            the qualifiers
+     * @param stereotypes
+     *            the stereotypes
+     * @param beanFactory
+     *            the beanfactory
+     */
+    public SpringBean(final String beanName, final Class<?> beanClass, final Set<Type> beanTypes,
+            final Set<Annotation> qualifiers, final Set<Class<? extends Annotation>> stereotypes,
+            final ConfigurableBeanFactory beanFactory) {
+        this.beanName = beanName;
+        this.beanClass = beanClass;
+        this.beanTypes = Collections.unmodifiableSet(beanTypes);
+        this.qualifiers = Collections.unmodifiableSet(qualifiers);
+        this.stereotypes = Collections.unmodifiableSet(stereotypes);
+        this.beanFactory = beanFactory;
+    }
+
+    /**
+     * Call the spring bean factory to get the bean.
+     */
+    @Override
+    public Object create(final CreationalContext<Object> context) {
+        LOGGER.info(MessageFormat.format("Create bean: {0}", this.beanName));
+        return this.beanFactory.getBean(this.beanName);
+    }
+
+    /**
+     * Call the spring bean factory to destroy the bean.
+     */
+    @Override
+    public void destroy(final Object instance, final CreationalContext<Object> context) {
+        LOGGER.info(MessageFormat.format("Destroy Bean: {0}", this.beanName));
+        this.beanFactory.destroyBean(this.beanName, instance);
+
+    }
+
+    @Override
+    public Set<Type> getTypes() {
+
+        return this.beanTypes;
+    }
+
+    @Override
+    public Set<Annotation> getQualifiers() {
+
+        return this.qualifiers;
+    }
+
+    @Override
+    public Class<? extends Annotation> getScope() {
+
+        return Dependent.class;
+    }
+
+    @Override
+    public String getName() {
+
+        return this.beanName;
+    }
+
+    @Override
+    public boolean isNullable() {
+
+        return true;
+    }
+
+    @Override
+    public Set<InjectionPoint> getInjectionPoints() {
+
+        return Collections.emptySet();
+    }
+
+    @Override
+    public Class<?> getBeanClass() {
+
+        return this.beanClass;
+    }
+
+    @Override
+    public Set<Class<? extends Annotation>> getStereotypes() {
+
+        return this.stereotypes;
+    }
+
+    @Override
+    public boolean isAlternative() {
+
+        return false;
+    }
+
+}
---
+++ src/main/java/de/javadesign/cdi/extension/spring/SpringBeanIntegrationExtension.java
@@ -0,0 +1,151 @@
+/*
+ * Copyright (C) 2010 Dirk Dreyer.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to
+ * in writing, software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations under the License.
+ */
+package de.javadesign.cdi.extension.spring;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
+import java.text.MessageFormat;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.enterprise.event.Observes;
+import javax.enterprise.inject.Any;
+import javax.enterprise.inject.Default;
+import javax.enterprise.inject.spi.AfterBeanDiscovery;
+import javax.enterprise.inject.spi.AnnotatedType;
+import javax.enterprise.inject.spi.Bean;
+import javax.enterprise.inject.spi.BeanManager;
+import javax.enterprise.inject.spi.Extension;
+import javax.enterprise.util.AnnotationLiteral;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.web.context.ContextLoader;
+
+/**
+ * CDI Extension to integrate Spring-Beans from Spring-Application-Context. <br />
+ * ATTENTION: Add Spring ApplicationContextListener-Config in the web.xml before any CDI-Implementation-Listener-Config.
+ * 
+ * @author Dirk Dreyer
+ */
+public class SpringBeanIntegrationExtension implements Extension {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger("de.javadesign.cdi.extension");
+
+    private static final Bean<?> CLASS_NOT_FOUND = null;
+
+    /**
+     * Default Constructor.
+     */
+    public SpringBeanIntegrationExtension() {
+        LOGGER.info(MessageFormat.format("{0} created.", this.getClass().getSimpleName()));
+    }
+
+    /**
+     * Listener method observes the AfterBeanDiscovery-Event.
+     * 
+     * @param event
+     *            the AfterBeanDiscovery Event
+     * @param beanManager
+     *            the BeanManager
+     */
+    public void connectCdiAndSpring(@Observes final AfterBeanDiscovery event, final BeanManager beanManager) {
+        final ConfigurableApplicationContext applicationContext = (ConfigurableApplicationContext) ContextLoader
+                .getCurrentWebApplicationContext();
+
+        if (applicationContext != null) {
+            LOGGER.info("ApplicationContext found.");
+            registerBeans(applicationContext, event, beanManager);
+        } else {
+            LOGGER.warn("No Spring-ApplicationContext found.");
+        }
+    }
+
+    /**
+     * Register found beans from the application context.
+     * 
+     * @param applicationContext
+     *            the spring application context
+     * @param event
+     *            the AfterBeanDiscoveryEvent
+     */
+    private void registerBeans(final ConfigurableApplicationContext applicationContext, final AfterBeanDiscovery event,
+            final BeanManager beanManager) {
+        final String[] beanDefinitionNames = applicationContext.getBeanDefinitionNames();
+        final ConfigurableListableBeanFactory beanFactory = applicationContext.getBeanFactory();
+        Bean<?> springBean = null;
+        for (final String beanName : beanDefinitionNames) {
+            final BeanDefinition beanDefinition = beanFactory.getBeanDefinition(beanName);
+            springBean = createBean(beanName, beanDefinition, beanFactory, beanManager);
+
+            if (springBean != null) {
+                event.addBean(springBean);
+            }
+        }
+
+    }
+
+    /**
+     * Try to create a bean if class is found.
+     * 
+     * @param beanName
+     *            the bean name to find and create
+     * @param beanDefinition
+     *            the spring bean definition
+     * @param beanManager
+     *            the injection bean manager
+     * @param beanFactory
+     *            a ConfigurableListableBeanFactory
+     * @return the created bean or null if class was not found
+     */
+    private Bean<?> createBean(final String beanName, final BeanDefinition beanDefinition,
+            final ConfigurableListableBeanFactory beanFactory, final BeanManager beanManager) {
+        Class<?> beanClass = null;
+
+        try {
+            beanClass = Class.forName(beanDefinition.getBeanClassName());
+        } catch (final ClassNotFoundException e) {
+            LOGGER.warn("Class {} not found.", beanName);
+            return CLASS_NOT_FOUND;
+        }
+
+        final AnnotatedType<?> annotatedType = beanManager.createAnnotatedType(beanClass);
+        final Set<Type> beanTypes = annotatedType.getTypeClosure();
+        final Set<Annotation> qualifiers = new HashSet<Annotation>();
+        qualifiers.add(new AnnotationLiteral<Any>() {
+
+            private static final long serialVersionUID = 1L;
+            // any implementation
+        });
+        qualifiers.add(new AnnotationLiteral<Default>() {
+
+            private static final long serialVersionUID = 1L;
+            // default implementation
+        });
+
+        final Set<Class<? extends Annotation>> stereotypes = new HashSet<Class<? extends Annotation>>();
+
+        for (final Annotation annotation : annotatedType.getAnnotations()) {
+            if (beanManager.isQualifier(annotation.annotationType())) {
+                qualifiers.add(annotation);
+            }
+            if (beanManager.isStereotype(annotation.annotationType())) {
+                stereotypes.add(annotation.annotationType());
+            }
+        }
+
+        return new SpringBean(beanName, beanClass, beanTypes, qualifiers, stereotypes, beanFactory);
+    }
+}
